f309ac8ef161ba281b2bb1bf5e7617e25a6a9d4e
Hui Lin Loo <huilinloo@Huis-MacBook-Pro.local>
2023-02-25 15:31:53 +0800

    added table to homepage

--- main-frontend/src/pages/home-page/home-page.jsx
+++ main-frontend/src/pages/home-page/home-page.jsx
@@ -1,9 +1,16 @@
 import "./home-page.css";
+import ClaimsTable from "../../components/claims-table"
+import ClaimsTestTable from "../../components/claims-table-test"
+import EditViewDialog from "../../components/edit-popup";
 
 const HomePage = () => {
     return <div className="hp-wrapper">
         <div className="hp-header-section">
             <h1 className="hp-header">Welcome to DBS</h1>
+        </div>
+        <div className="table">
+            <ClaimsTestTable/>
+            <EditViewDialog />
         </div>
 
 
