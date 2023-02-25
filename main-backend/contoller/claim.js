81a6582f44efe5f5d600760bfb63dfe9a42d9f48
maxlee <max.lee98@gmail.com>
2023-02-25 14:29:24 +0800

    Claims API
    Posting a claim, Getting all claims for an EmployeeID

--- main-backend/contoller/claim.js
+++ main-backend/contoller/claim.js
@@ -30,6 +30,34 @@ function getPolicies(req, res) {
   );
 }
 
+// router.get("/api/get-account-information-account/:accountId", (req, res) => {
+
+function getClaims(req, res) {
+  const employeeid = req.params.employeeid;
+  const sql = `
+    SELECT c.*
+    FROM public."InsuranceClaims" c
+    JOIN public."insurancepolicies" p ON c.insuranceid = p.insuranceid
+    JOIN public."User" e ON p.employeeid = e.employeeid
+    WHERE e.employeeid = $1;
+  `;
+  const values = [employeeid];
+  req.con.query(sql, values, function (err, result) {
+    if (err) {
+      console.error(err);
+      res
+        .status(500)
+        .send({ message: "Failed to retrieve Claims for EmployeeID" });
+    }
+
+    if (result.length > 0) {
+      res.send(result);
+    } else {
+      res.status(404).send({ message: "No Claims  found for the given user." });
+    }
+  });
+}
+
 function sendClaim(req, res) {
   // Verify that such a user exist
   const { insuranceid, firstname, lastname, expensedate, amount, purpose } =
@@ -46,18 +74,21 @@ function sendClaim(req, res) {
     purpose,
   ];
 
-  req.pool.query(sql, insertValues, (error, result) => {
-    if (error) console.log(error);
-    if (result.length > 0) {
-      res.send(result);
-    } else {
-      res.status(404).send({
-        message: "No policies found for the given user.",
-      });
+  req.con.query(sql, values, function (err, result) {
+    if (err) {
+      console.error(err);
+      res.status(500).send({ message: "Failed to create Claim transaction" });
+      return;
     }
+    console.log("Claim has been successfully posted to the database");
+    res.send({
+      message: `Claim has been successfully posted to the database with Debit = ${debit}, Credit = ${credit}, Amount = ${amount}`,
+    });
   });
 }
 
 module.exports = {
   getPolicies,
+  getClaims,
+  sendClaim,
 };
--- main-backend/router/insurance.js
+++ main-backend/router/insurance.js
@@ -4,6 +4,7 @@ const claimController = require("../contoller/claim");
 const router = express.Router();
 
 router.get("/api/get-policies", claimController.getPolicies);
-router.post();
+router.get("/api/get-claims/:employeeid", claimController.getClaims);
+router.post("/api/post-claim", claimController.sendClaim);
 
 module.exports = router;
