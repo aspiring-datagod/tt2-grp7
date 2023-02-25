439134ec7a7462bf78b15b97b112d7abfb6b4183
maxlee <max.lee98@gmail.com>
2023-02-25 10:21:53 +0800

    Add express index.js
    Initial Commit for index.js backend

---
+++ main-backend/index.js
@@ -0,0 +1,55 @@
+const express = require("express");
+const cors = require("cors");
+const bodyParser = require("body-parser");
+const mysql = require("mysql");
+const session = require("express-session");
+
+// Controller Modules
+
+// const authentication = require("./controllers/authentication");
+// const account = require("./controllers/account");
+// const transaction = require("./controllers/transaction");
+
+// var con = mysql.createConnection({
+//   host: "localhost",
+//   user: "",
+//   password: "",
+//   database: "",
+// });
+
+// con.connect(function (err) {
+//   if (err) throw err;
+//   console.log("Connected!");
+// });
+
+const app = express();
+const port = process.env.PORT || 4000;
+
+app.use(
+  session({
+    secret: "secret-key",
+    resave: false,
+    saveUninitialized: false,
+  })
+);
+
+app.use(cors());
+app.use(bodyParser.json({ limit: "50mb" }));
+app.use(bodyParser.urlencoded({ limit: "50mb", extended: true }));
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+app.use(function (req, res, next) {
+  req.con = con;
+  next();
+});
+
+// app.use("/", authentication);
+// app.use("/", account);
+// app.use("/", transaction);
+
+app.listen(port, () => {
+  console.log(`Express Bank app listening on port ${port}`);
+});
